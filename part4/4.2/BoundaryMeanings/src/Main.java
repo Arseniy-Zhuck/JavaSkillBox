50d78ed4d1188e58bbf0a3d9cd85d8d5fe1f4d7a
Unknown <arseniyzhuck@mail.ru>
2020-06-21 09:43:50 +0300

    Стыдно HomeWork4.2

--- part4/4.2/BoundaryMeanings/src/Main.java
+++ part4/4.2/BoundaryMeanings/src/Main.java
@@ -11,6 +11,6 @@ public class Main {
         System.out.println("Float max: " + Float.MAX_VALUE);
         System.out.println("Float min: " + Float.MIN_VALUE);
         System.out.println("Double max: " + Double.MAX_VALUE);
-        System.out.println("Double min: " + Double.MAX_VALUE);
+        System.out.println("Double min: " + Double.MIN_VALUE);
     }
 }
