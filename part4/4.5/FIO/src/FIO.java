18c63f253806ef1e62f7f8d4ea464bae2e966d74
Unknown <arseniyzhuck@mail.ru>
2020-07-12 18:00:08 +0300

    НоmeWork4.5_AdditionalTask

--- part4/4.5/FIO/src/FIO.java
+++ part4/4.5/FIO/src/FIO.java
@@ -8,12 +8,21 @@ public class FIO {
         System.out.println("Введите строку");
         String s = (new BufferedReader(new InputStreamReader(System.in))).readLine();
         String fullName = s.trim();
+        fullName = fullName.replaceAll("\\s*-\\s*","-");
+        System.out.println(fullName);
         String[] ar;
-        if(!Pattern.compile("[А-ЯЁ&&[^ЪЬЫ]][а-яё]+\\s[А-ЯЁ&&[^ЪЬЫ]][а-яё]+\\s" +
-                "[А-ЯЁ&&[^ЪЬЫ]][а-яё]+$").matcher(fullName).matches())
+        //ar = s.trim().split("\\s+", 3);
+        //System.out.println("Фамилия: " + ar[0]);
+        //System.out.println(Pattern.compile("([А-ЯЁ&&[^ЪЬЫ]][а-яё]+)||([А-ЯЁ&&[^ЪЬЫ]][а-яё]+-[А-ЯЁ&&[^ЪЬЫ]][а-яё]+)").matcher(ar[0].matches());
+        //System.out.println("Имя: " + ar[1]);
+        //System.out.println("Отчество: " + ar[2]);
+
+        if(!Pattern.compile("(([А-ЯЁ&&[^ЪЬЫ]][а-яё]+)||([А-ЯЁ&&[^ЪЬЫ]][а-яё]+-[А-ЯЁ&&[^ЪЬЫ]][а-яё]+))\\s" +
+                "(([А-ЯЁ&&[^ЪЬЫ]][а-яё]+)||([А-ЯЁ&&[^ЪЬЫ]][а-яё]+-[А-ЯЁ&&[^ЪЬЫ]][а-яё]+))\\s" +
+                "(([А-ЯЁ&&[^ЪЬЫ]][а-яё]+)||([А-ЯЁ&&[^ЪЬЫ]][а-яё]+\\s[а-яё&&[^ъьы]][а-яё]+))$").matcher(fullName).matches())
             System.out.println("Its all bad");
         else {
-            ar = s.trim().split("\\s+");
+            ar = fullName.trim().split("\\s+", 3);
             System.out.println("Фамилия: " + ar[0]);
             System.out.println("Имя: " + ar[1]);
             System.out.println("Отчество: " + ar[2]);
