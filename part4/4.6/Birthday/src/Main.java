5cb8ce5de21dc6d31d2bde953433653817af4a10
Unknown <arseniyzhuck@mail.ru>
2020-07-13 17:51:24 +0300

    HomeWork4.6

---
+++ part4/4.6/Birthday/src/Main.java
@@ -0,0 +1,25 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        int day = 8;
+        int month = 12;
+        int year = 1990;
+        calendar.set(year,month - 1,day);
+        DateFormat dF = new SimpleDateFormat("dd.MM.YYYY - EEE", Locale.ENGLISH);
+        Calendar currentCalendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        int i = 0;
+        while (date.getTime() < currentCalendar.getTime().getTime()) {
+
+            System.out.println((i++) + " - " + dF.format(date));
+            calendar.add(Calendar.YEAR, 1);
+            date = calendar.getTime();
+        }
+    }
+}
