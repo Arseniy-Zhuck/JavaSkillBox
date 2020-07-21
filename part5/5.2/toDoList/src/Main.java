db216b4ad09519fe2b0c2705ff3e8c9636eba527
Arseniy-Zhuck <arseniyzhuck@mail.ru>
2020-07-21 15:40:19 +0300

    HomeWork5.2

---
+++ part5/5.2/toDoList/src/Main.java
@@ -0,0 +1,143 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void printToDoList(ArrayList<String> arrayList) {
+        System.out.println(String.format("You have %d deals you must finish.", arrayList.size()));
+        for (String deal : arrayList) System.out.println(deal);
+    }
+
+    public static void addDeal(ArrayList<String> arrayList, String[] params) {
+        String s = "";
+        try {
+            s = params[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR!!! You write no number and no deal");
+            return;
+        }
+
+        Boolean withNumber = true;
+
+        int number = 0;
+        try {
+            number = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            withNumber = false;
+        }
+
+        if (withNumber) {
+            String deal;
+            try {
+                deal = params[2];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("ERROR!!! You write no deal in command");
+                return;
+            }
+            try {
+                arrayList.add(number, deal);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("ERROR!!! You write wrong number");
+                return;
+            }
+        } else {
+            if (params.length == 3) s += " " + params[2];
+            arrayList.add(s);
+        }
+    }
+
+    public static void editDeal(ArrayList<String> arrayList, String[] params) {
+
+
+        String s = "";
+        try {
+            s = params[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR!!! You write no number and no deal");
+            return;
+        }
+
+        int number = 0;
+        try {
+            number = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR!!! You write no position to edit");
+            return;
+        }
+
+        String deal = "";
+        try {
+            deal = params[2];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR!!! You write no deal in command");
+        }
+
+        try {
+            arrayList.set(number, deal);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ERROR!!! You write wrong number");
+            return;
+        }
+    }
+
+    public static void deleteDeal(ArrayList<String> arrayList, String[] params) {
+        if (params.length > 2) {
+            System.out.println("ERROR!!! DELETE command must have onle number");
+            return;
+        }
+
+        String s = "";
+        try {
+            s = params[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR!!! You write no number");
+            return;
+        }
+
+        int number = 0;
+        try {
+            number = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR!!! You write no position to delete");
+            return;
+        }
+
+        try {
+            arrayList.remove(number);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ERROR!!! You write wrong number");
+            return;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> toDoList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (;;) {
+            System.out.println("/-----------------------------------------------------------------------------/");
+            System.out.println("Please, type one of commands: ");
+            System.out.println("LIST");
+            System.out.println("ADD Какое-то дело");
+            System.out.println("ADD 4 Какое-то дело на четвёртом месте");
+            System.out.println("EDIT 3 Новое название дела");
+            System.out.println("DELETE 7");
+            System.out.println("EXIT");
+            System.out.println("/-----------------------------------------------------------------------------/");
+            String command = reader.readLine().trim();
+            String[] commandComponents = command.split(" ", 3);
+            if (commandComponents[0].equals("LIST")) printToDoList(toDoList);
+            else if (commandComponents[0].equals("ADD")) addDeal(toDoList, commandComponents);
+            else if (commandComponents[0].equals("EDIT")) editDeal(toDoList, commandComponents);
+            else if (commandComponents[0].equals("DELETE")) deleteDeal(toDoList, commandComponents);
+            else if (commandComponents[0].equals("EXIT")) break;
+            else System.out.println("You write uknown command");
+
+        }
+    }
+
+
+
+}
